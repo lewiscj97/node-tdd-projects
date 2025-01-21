2a5ba9ce42ba84162c7c09ceeb7ae15cfde5273b
lewis.jones <lewis1997@live.com>
2025-01-21 17:57:47 +0000

    feat: add functionality to create frames from input

--- test/test.js
+++ test/test.js
@@ -5,9 +5,9 @@ describe('Service test', () => {
 
   it('should return hello world', () => {
     const input = 'X 45 4/ 32';
-    const expected = [['X'], [4, 5], [4, '/'], [3, 2]];
+    const expected = [['X'], ['4', '5'], ['4', '/'], ['3', '2']];
     const output = processInput(input);
 
-    expect(output).to.eq(expected);
+    expect(output).to.deep.eq(expected);
   });
 });
