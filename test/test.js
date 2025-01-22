2ecd4194bd2eeab42e3dc7cd1819a2eeb806e3ef
lewis.jones <lewis1997@live.com>
2025-01-22 20:14:37 +0000

    feat: add remaining numbers

--- test/test.js
+++ test/test.js
@@ -41,4 +41,82 @@ describe('Number to LCD', () => {
     const output = numberToLcd(input);
     expect(output).to.deep.eq(expected);
   });
+
+  it('should return correct value for 4', () => {
+    const input = 4;
+    const expected =
+      [
+        ['   '],
+        ['|_|'],
+        ['  |']
+      ];
+
+    const output = numberToLcd(input);
+    expect(output).to.deep.eq(expected);
+  });
+
+  it('should return correct value for 5', () => {
+    const input = 5;
+    const expected =
+      [
+        [' _ '],
+        ['|_ '],
+        [' _|']
+      ];
+
+    const output = numberToLcd(input);
+    expect(output).to.deep.eq(expected);
+  });
+
+  it('should return correct value for 6', () => {
+    const input = 6;
+    const expected =
+      [
+        [' _ '],
+        ['|_ '],
+        ['|_|']
+      ];
+
+    const output = numberToLcd(input);
+    expect(output).to.deep.eq(expected);
+  });
+
+  it('should return correct value for 7', () => {
+    const input = 7;
+    const expected =
+      [
+        [' _ '],
+        ['  |'],
+        ['  |']
+      ];
+
+    const output = numberToLcd(input);
+    expect(output).to.deep.eq(expected);
+  });
+
+  it('should return correct value for 8', () => {
+    const input = 8;
+    const expected =
+      [
+        [' _ '],
+        ['|_|'],
+        ['|_|']
+      ];
+
+    const output = numberToLcd(input);
+    expect(output).to.deep.eq(expected);
+  });
+
+  it('should return correct value for 9', () => {
+    const input = 9;
+    const expected =
+      [
+        [' _ '],
+        ['|_|'],
+        [' _|']
+      ];
+
+    const output = numberToLcd(input);
+    expect(output).to.deep.eq(expected);
+  });
 });
