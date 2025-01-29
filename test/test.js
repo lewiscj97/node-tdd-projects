69873d496f655489c74cfa4a8ad25711fc1dd242
lewis.jones <lewis1997@live.com>
2025-01-29 18:40:14 +0000

    feat: functionality for single day of regular rental

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
 const expect = require('chai').expect;
-const { helloWorld } = require('../lib/index');
+const { calculateRentalSummary } = require('../lib/index');
 
 describe('Video store', () => {
   describe('Price and point calculation', () => {
     it('should calculate price and points for a regular movie for 1 day', () => {
       const movie = {
-        name: "Crazynotes",
-        type: "regular",
+        name: 'Crazynotes',
+        type: 'regular',
         days: 1
       }
 
       const rentalSummary = calculateRentalSummary(movie);
 
-      expect(priceAndPoints.price).to.eq(2.0);
-      expect(priceAndPoints.points).to.eq(1);
+      expect(rentalSummary.price).to.eq(2.0);
+      expect(rentalSummary.points).to.eq(1);
     });
   });
 });
