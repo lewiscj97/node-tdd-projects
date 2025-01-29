60eb142fb5fde1ba0cbec50698b71e7543b459cd
lewis.jones <lewis1997@live.com>
2025-01-29 18:50:59 +0000

    refactor: movie class, constants file

--- test/test.js
+++ test/test.js
@@ -1,14 +1,11 @@
 const expect = require('chai').expect;
 const { calculateRentalSummary } = require('../lib/index');
+const MovieRental = require('../lib/MovieRental');
 
 describe('Video store', () => {
   describe('Price and point calculation, single rental', () => {
     it('should calculate price and points - regular movie - 1 day', () => {
-      const movie = {
-        name: 'Crazynotes',
-        type: 'regular',
-        days: 1
-      }
+      const movie = new MovieRental('Crazynotes', 'regular', 1);
 
       const rentalSummary = calculateRentalSummary(movie);
 
@@ -17,11 +14,7 @@ describe('Video store', () => {
     });
 
     it('should calculate price and points - regular movie - 2 days', () => {
-      const movie = {
-        name: 'Crazynotes',
-        type: 'regular',
-        days: 2
-      }
+      const movie = new MovieRental('Crazynotes', 'regular', 2);
 
       const rentalSummary = calculateRentalSummary(movie);
 
@@ -30,11 +23,7 @@ describe('Video store', () => {
     });
 
     it('should calculate price and points - regular movie - 3 days', () => {
-      const movie = {
-        name: 'Crazynotes',
-        type: 'regular',
-        days: 3
-      }
+      const movie = new MovieRental('Crazynotes', 'regular', 3);
 
       const rentalSummary = calculateRentalSummary(movie);
 
@@ -43,11 +32,7 @@ describe('Video store', () => {
     });
 
     it('should calculate price and points - regular movie - 3 days', () => {
-      const movie = {
-        name: 'Crazynotes',
-        type: 'regular',
-        days: 4
-      }
+      const movie = new MovieRental('Crazynotes', 'regular', 4);
 
       const rentalSummary = calculateRentalSummary(movie);
 
