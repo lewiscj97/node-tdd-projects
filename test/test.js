72fee3ce6a3862db437958e4607fe9e64fe7db79
lewis.jones <lewis1997@live.com>
2025-01-29 18:51:51 +0000

    refactor: movie class, constants file

--- test/test.js
+++ test/test.js
@@ -1,43 +1,44 @@
 const expect = require('chai').expect;
 const { calculateRentalSummary } = require('../lib/index');
 const MovieRental = require('../lib/MovieRental');
+const { REGULAR_TYPE, REGULAR_MOVIE_POINTS } = require('../lib/constants');
 
 describe('Video store', () => {
   describe('Price and point calculation, single rental', () => {
     it('should calculate price and points - regular movie - 1 day', () => {
-      const movie = new MovieRental('Crazynotes', 'regular', 1);
+      const movie = new MovieRental('Crazynotes', REGULAR_TYPE, 1);
 
       const rentalSummary = calculateRentalSummary(movie);
 
       expect(rentalSummary.price).to.eq(2.0);
-      expect(rentalSummary.points).to.eq(1);
+      expect(rentalSummary.points).to.eq(REGULAR_MOVIE_POINTS);
     });
 
     it('should calculate price and points - regular movie - 2 days', () => {
-      const movie = new MovieRental('Crazynotes', 'regular', 2);
+      const movie = new MovieRental('Crazynotes', REGULAR_TYPE, 2);
 
       const rentalSummary = calculateRentalSummary(movie);
 
       expect(rentalSummary.price).to.eq(2.0);
-      expect(rentalSummary.points).to.eq(1);
+      expect(rentalSummary.points).to.eq(REGULAR_MOVIE_POINTS);
     });
 
     it('should calculate price and points - regular movie - 3 days', () => {
-      const movie = new MovieRental('Crazynotes', 'regular', 3);
+      const movie = new MovieRental('Crazynotes', REGULAR_TYPE, 3);
 
       const rentalSummary = calculateRentalSummary(movie);
 
       expect(rentalSummary.price).to.eq(3.5);
-      expect(rentalSummary.points).to.eq(1);
+      expect(rentalSummary.points).to.eq(REGULAR_MOVIE_POINTS);
     });
 
     it('should calculate price and points - regular movie - 3 days', () => {
-      const movie = new MovieRental('Crazynotes', 'regular', 4);
+      const movie = new MovieRental('Crazynotes', REGULAR_TYPE, 4);
 
       const rentalSummary = calculateRentalSummary(movie);
 
       expect(rentalSummary.price).to.eq(5);
-      expect(rentalSummary.points).to.eq(1);
+      expect(rentalSummary.points).to.eq(REGULAR_MOVIE_POINTS);
     });
   });
 });
