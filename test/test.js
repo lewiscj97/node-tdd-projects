f516d69529b337b8d35c4b7e8d3312da88e0f2d1
lewis.jones <lewis1997@live.com>
2025-01-29 18:41:19 +0000

    test: regular movie 2 days

--- test/test.js
+++ test/test.js
@@ -2,12 +2,25 @@ const expect = require('chai').expect;
 const { calculateRentalSummary } = require('../lib/index');
 
 describe('Video store', () => {
-  describe('Price and point calculation', () => {
-    it('should calculate price and points for a regular movie for 1 day', () => {
+  describe('Price and point calculation, single rental', () => {
+    it('should calculate price and points - regular movie - 1 day', () => {
       const movie = {
         name: 'Crazynotes',
         type: 'regular',
         days: 1
+      }
+
+      const rentalSummary = calculateRentalSummary(movie);
+
+      expect(rentalSummary.price).to.eq(2.0);
+      expect(rentalSummary.points).to.eq(1);
+    });
+
+    it('should calculate price and points - regular movie - 2 days', () => {
+      const movie = {
+        name: 'Crazynotes',
+        type: 'regular',
+        days: 2
       }
 
       const rentalSummary = calculateRentalSummary(movie);
