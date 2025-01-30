7494d3efbb47a08d028558b47166fb4464154eac
lewis.jones <lewis1997@live.com>
2025-01-30 19:33:09 +0000

    test: get tennis score

--- test/test.js
+++ test/test.js
@@ -4,15 +4,26 @@ const Player = require('../lib/Player');
 
 describe('Tennis scores', () => {
 
-  const tennis = new Tennis();
   const player1 = new Player('Rodger');
   const player2 = new Player('Andy');
+
+  const tennis = new Tennis(player1, player2);
 
   it('should be able to score for a player', () => {
     tennis.score(player1);
 
     const expected = 1;
     const score = player1.getScore();
+
+    expect(expected).to.eq(score);
+  });
+
+  it('should give the score in tennis terms', () => {
+    tennis.score(player1);
+    tennis.score(player2);
+
+    const expected = "15,15";
+    const score = tennis.getScore();
 
     expect(expected).to.eq(score);
   });
