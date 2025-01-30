49bf1e6b92930fc815ee83eebd978e2a5fa9fc44
lewis.jones <lewis1997@live.com>
2025-01-30 19:38:54 +0000

    feat: scoring for not deuce or winning

--- test/test.js
+++ test/test.js
@@ -4,10 +4,15 @@ const Player = require('../lib/Player');
 
 describe('Tennis scores', () => {
 
-  const player1 = new Player('Rodger');
-  const player2 = new Player('Andy');
+  let player1;
+  let player2;
+  let tennis;
 
-  const tennis = new Tennis(player1, player2);
+  beforeEach(() => {
+    player1 = new Player('Rodger');
+    player2 = new Player('Andy');
+    tennis = new Tennis(player1, player2);
+  });
 
   it('should be able to score for a player', () => {
     tennis.score(player1);
@@ -22,7 +27,7 @@ describe('Tennis scores', () => {
     tennis.score(player1);
     tennis.score(player2);
 
-    const expected = "15,15";
+    const expected = '15,15';
     const score = tennis.getScore();
 
     expect(expected).to.eq(score);
