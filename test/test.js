1fe26ec92d371ab464c7582f481702c194bcdbce
lewis.jones <lewis1997@live.com>
2025-01-30 19:29:42 +0000

    feat: player can score

--- test/test.js
+++ test/test.js
@@ -1,12 +1,19 @@
 const expect = require('chai').expect;
-const { helloWorld } = require('../lib/index');
+const Tennis = require('../lib/Tennis');
+const Player = require('../lib/Player');
 
-describe('Service test', () => {
+describe('Tennis scores', () => {
 
-  it('should return hello world', () => {
-    const expected = 'Hello, World!';
-    const output = helloWorld();
+  const tennis = new Tennis();
+  const player1 = new Player('Rodger');
+  const player2 = new Player('Andy');
 
-    expect(output).to.eq(expected);
+  it('should be able to score for a player', () => {
+    tennis.score(player1);
+
+    const expected = 1;
+    const score = player1.getScore();
+
+    expect(expected).to.eq(score);
   });
 });
